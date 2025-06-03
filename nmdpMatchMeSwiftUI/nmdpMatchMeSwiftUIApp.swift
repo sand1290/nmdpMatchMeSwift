0e295e274643d76af86df5a97e26deea6771a3e6
jbrelsf2 <jbrelsf2@nmdp.org>
2025-06-03 14:22:04 -0500

    Initial Commit

---
+++ nmdpMatchMeSwiftUI/nmdpMatchMeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  nmdpMatchMeSwiftUIApp.swift
+//  nmdpMatchMeSwiftUI
+//
+//  Created by Jason Brelsford on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct nmdpMatchMeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nmdpMatchMeSwiftUITests/nmdpMatchMeSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  nmdpMatchMeSwiftUITests.swift
+//  nmdpMatchMeSwiftUITests
+//
+//  Created by Jason Brelsford on 6/3/25.
+//
+
+import Testing
+@testable import nmdpMatchMeSwiftUI
+
+struct nmdpMatchMeSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
